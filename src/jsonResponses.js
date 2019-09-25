46007da35ebe1d483a1ac9f797e550db2043e534
Ella Mathewson <ellaofohio@gmail.com>
2019-09-25 08:30:32 -0400

    Set up page code with basic structure of code needed

--- src/jsonResponses.js
+++ src/jsonResponses.js
@@ -1,9 +1,21 @@
+/* eslint-disable no-console */
 /* eslint-disable linebreak-style */
 const users = {};
 
 const respondJSON = (request, response, status, object) => {
-  response.writeHead(status, { 'Content-Type': 'application/json' });
+  const headers = {
+    'Content-Type': 'application/json',
+  };
+  response.writeHead(status, headers);
   response.write(JSON.stringify(object));
+  response.end();
+};
+
+const respondJSONMeta = (request, response, status) => {
+  const headers = {
+    'Content-Type': 'application/json',
+  };
+  response.writeHead(status, headers);
   response.end();
 };
 
@@ -11,10 +23,45 @@ const getUsers = (request, response) => {
   const responseObjectJSON = {
     users,
   };
-
+  console.dir(responseObjectJSON);
   respondJSON(request, response, 200, responseObjectJSON);
 };
 
+const getUsersMeta = (request, response) => respondJSONMeta(request, response, 200);
+
+const notReal = (request, response) => {
+  const responseObjectJSON = {
+    message: 'Error: 404 not found',
+    id: 'notFound',
+  };
+  respondJSON(request, response, 404, responseObjectJSON);
+};
+
+const notRealMeta = (request, response) => respondJSONMeta(request, response, 404);
+
+const notFound = (request, response) => {
+  // create error message for response
+  const responseJSON = {
+    message: 'The page you are looking for was not found.',
+    id: 'notFound',
+  };
+
+  // return a 404 with an error message
+  respondJSON(request, response, 404, responseJSON);
+};
+
+// function for 404 not found without message
+const notFoundMeta = (request, response) => {
+  // return a 404 without an error message
+  respondJSONMeta(request, response, 404);
+};
+
+
 module.exports = {
   getUsers,
+  getUsersMeta,
+  notReal,
+  notRealMeta,
+  notFound,
+  notFoundMeta,
 };
--- src/server.js
+++ src/server.js
@@ -8,59 +8,51 @@ const jsonHandler = require('./jsonResponses.js');
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const urlStruct = {
-    'GET': {
-        '/': htmlHandler.getIndex,
-        '/style.css': htmlHandler.getCSS,
-        '/getUsers': jsonHandler.getUsers,
-        '/notReal': jsonHandler.notReal,
-    },
-    'HEAD': {
-        '/getUsers': jsonHandler.getUsersMeta,
-        '/notReal': jsonHandler.notRealMeta,
-    }
-}
+  GET: {
+    '/': htmlHandler.getIndex,
+    '/style.css': htmlHandler.getCSS,
+    '/getUsers': jsonHandler.getUsers,
+    '/notReal': jsonHandler.notReal,
+    notFound: jsonHandler.notFound,
+  },
+  HEAD: {
+    '/getUsers': jsonHandler.getUsersMeta,
+    '/notReal': jsonHandler.notRealMeta,
+    notFound: jsonHandler.notFoundMeta,
+  },
+};
 
-const handlePost = (request, response, parsedUrl) => {
-    if (parsedUrl.pathname === '/addUser') {
-        const res = response;
-        const body = [];
+// const handlePost = (request, response, parsedUrl) => {
+//   if (parsedUrl.pathname === '/addUser') {
+//     const res = response;
+//     const body = [];
 
-        request.on('error', (err) => {
-            console.dir(err);
-            res.statusCode = 400;
-            res.end();
-        });
+//     request.on('error', (err) => {
+//       console.dir(err);
+//       res.statusCode = 400;
+//       res.end();
+//     });
 
-        request.on('data', (chunk) => {
-            body.push(chunk);
-        });
+//     request.on('data', (chunk) => {
+//       body.push(chunk);
+//     });
 
-        request.on('end', () => {
-            const bodyString = Buffer.concat(body).toString();
-            const bodyParams = query.parse(bodyString);
-            jsonHandler.addUser(request, res, bodyParams);
-        })
-    }
-}
+//     request.on('end', () => {
+//       const bodyString = Buffer.concat(body).toString();
+//       const bodyParams = query.parse(bodyString);
+//       jsonHandler.addUser(request, res, bodyParams);
+//     });
+//   }
+// };
 
 const onRequest = (request, response) => {
   const parsedUrl = url.parse(request.url);
 
-//   switch (parsedUrl.pathname) {
-//     case '/':
-//       htmlHandler.getIndex(request, response);
-//       break;
-//     case '/style.css':
-//       htmlHandler.getCSS(request, response);
-//       break;
-//     case '/getUsers':
-//         break;
-//     case 'notReal':
-//         break;
-//     default:
-//       htmlHandler.getIndex(request, response);
-//       break;
-//   }
+  if (urlStruct[request.method][parsedUrl.pathname]) {
+    urlStruct[request.method][parsedUrl.pathname](request, response);
+  } else {
+    urlStruct[request.method].notFound(request, response);
+  }
 };
 
 http.createServer(onRequest).listen(port);
