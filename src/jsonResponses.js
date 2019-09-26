b2ea1adedc7c091561c1caaff4a050c5a587768d
Ella Mathewson <ellaofohio@gmail.com>
2019-09-25 22:05:39 -0400

    Got main functionality working, still missing some components

--- src/jsonResponses.js
+++ src/jsonResponses.js
@@ -97,29 +97,19 @@ module.exports = {
   notFoundMeta,
 };
 */
-
-// Note this object is purely in memory
-// When node shuts down this will be cleared.
-// Same when your heroku app shuts down from inactivity
-// We will be working with databases in the next few weeks.
 const users = {};
 
-// function to respond with a json object
-// takes request, response, status code and object to send
 const respondJSON = (request, response, status, object) => {
   response.writeHead(status, { 'Content-Type': 'application/json' });
   response.write(JSON.stringify(object));
   response.end();
 };
 
-// function to respond without json body
-// takes request, response and status code
 const respondJSONMeta = (request, response, status) => {
   response.writeHead(status, { 'Content-Type': 'application/json' });
   response.end();
 };
 
-// return user object as JSON
 const getUsers = (request, response) => {
   const responseJSON = {
     users,
@@ -128,9 +118,10 @@ const getUsers = (request, response) => {
   respondJSON(request, response, 200, responseJSON);
 };
 
+const getUsersMeta = (request, response) => respondJSONMeta(request, response, 200);
+
 // function to add a user from a POST body
 const addUser = (request, response, body) => {
-  // default json message
   const responseJSON = {
     message: 'Name and age are both required.',
   };
@@ -160,20 +151,45 @@ const addUser = (request, response, body) => {
   users[body.name].name = body.name;
   users[body.name].age = body.age;
 
-  // if response is created, then set our created message
-  // and sent response with a message
   if (responseCode === 201) {
     responseJSON.message = 'Created Successfully';
     return respondJSON(request, response, responseCode, responseJSON);
   }
-  // 204 has an empty payload, just a success
-  // It cannot have a body, so we just send a 204 without a message
-  // 204 will not alter the browser in any way!!!
   return respondJSONMeta(request, response, responseCode);
+};
+
+const notReal = (request, response) => {
+  const responseJSON = {
+    message: 'The page you are looking for was not found',
+    id: 'notFound',
+  };
+  respondJSON(request, response, 404, responseJSON);
+};
+
+const notRealMeta = (request, response) => respondJSONMeta(request, response, 404);
+
+const notFound = (request, response) => {
+  // create error message for response
+  const responseJSON = {
+    message: 'The page you are looking for was not found.',
+    id: 'notFound',
+  };
+
+  // return a 404 with an error message
+  respondJSON(request, response, 404, responseJSON);
+};
+
+const notFoundMeta = (request, response) => {
+  respondJSONMeta(request, response, 404);
 };
 
 // public exports
 module.exports = {
   getUsers,
+  getUsersMeta,
   addUser,
+  notReal,
+  notRealMeta,
+  notFound,
+  notFoundMeta,
 };
